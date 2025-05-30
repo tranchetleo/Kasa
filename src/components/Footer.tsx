dc8cd0bad66d6322f7dab280d258a52719e2646c
Léo Tranchet <tranchetleo@gmail.com>
2025-05-30 17:44:30 +0200

    feat: add Footer component with styling and content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,15 @@
+import "./Footer.css";
+import logoWhite from "../assets/images/logo-white.png";
+
 const Footer = () => {
   return (
-    <>
-    </>
+    <footer className="footer">
+      <img src={logoWhite} alt="" />
+      <div className="footer__line">
+        <p className="footer__text">© 2023 Kasa.</p>
+        <p className="footer__text">All rights reserved</p>
+      </div>
+    </footer>
   );
 };
 
