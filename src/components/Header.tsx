d2e38ea96f0feb06f9a00fa2b78c6e0644beb383
Léo Tranchet <tranchetleo@gmail.com>
2025-05-30 17:46:25 +0200

    feat: implement Header component with navigation and styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,25 @@
+import "./Header.css";
+import logo from "../assets/images/logo.png";
+
 const Header = () => {
   return (
-    <>
-    </>
+    <header>
+      <div className="navbar">
+        <a href="/">
+          <img src={logo} alt="" />
+        </a>
+        <nav>
+          <ul>
+            <li>
+              <a href="/">Accueil</a>
+            </li>
+            <li>
+              <a href="/a-propos">A Propos</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
   );
 };
 
