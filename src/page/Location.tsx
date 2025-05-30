675692eeae6a5542a3747f3ae863f25ecc138247
Léo Tranchet <tranchetleo@gmail.com>
2025-05-30 20:35:06 +0200

    feat: update package.json and package-lock.json to include gh-pages dependency and deployment scripts; enhance Location component with error handling and image display

--- src/page/Location.tsx
+++ src/page/Location.tsx
@@ -1,8 +1,32 @@
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import locations from "../data/logement.json"; // adjust the path as needed
+
 const Location = () => {
+  const { id } = useParams<{ id: string }>();
+  console.log("Location ID:", id);
+  const navigate = useNavigate();
+  const location = locations.find((loc) => loc.id === id);
+
+  useEffect(() => {
+    if (!location) {
+      navigate("/404", { replace: true });
+    }
+  }, [location, navigate]);
   return (
-    <>
-      <h1>Location</h1>
-    </>
+    <div className="location">
+      {location && location.pictures && location.pictures.length > 0 && (
+        <div className="carousel">
+          <img
+            src={location.pictures[0]}
+            alt={`Location ${location.title} - 1`}
+            style={{ width: "100%", height: "auto" }}
+          />
+          
+        </div>
+      )}
+    </div>
   );
 };
 
