2aa15cb4cf5d1e2cf71263cc68a44469df3ffc24
Léo Tranchet <tranchetleo@gmail.com>
2025-05-30 16:01:24 +0200

    feat: add Header and Footer components to Router

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Header from '../components/Header.tsx'
+import Footer from '../components/Footer.tsx'
 import Home from '../page/Home.tsx'
 import About from '../page/About.tsx'
 import Location from '../page/Location.tsx'
@@ -6,12 +8,14 @@ import Location from '../page/Location.tsx'
 function Router() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/a-propos" element={<About />} />
         <Route path="/location/:id" element={<Location />} />
         <Route path="*" element={<h1>404 Not Found</h1>} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
